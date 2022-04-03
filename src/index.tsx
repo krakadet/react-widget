17a1d3c357d7f91d4ce1728b972205648c3ddf8f
mkovtun <mkovtun@magnite.com>
2022-04-03 23:32:22 +0200

    update poll widget

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,15 @@ import "./style.scss";
 
 const config={
     mainQuestion: "What is your favorite color?",
-    answers: ['Red', 'Green', 'Blue']
+    answers: ['Red', 'Green', 'Blue'],
+    elementId: 'app'
 }
 
 if (process.env.NODE_ENV === 'development') {
-    ReactDOM.render(<PollWidget {...config}   />, document.getElementById('app'))
+    ReactDOM.render(<PollWidget {...config}   />, document.getElementById(config.elementId))
 }
 
 
 export const init = (config: any) => {
-    ReactDOM.render(<PollWidget {...config}   />, document.getElementById('app'));
+    ReactDOM.render(<PollWidget {...config}   />, document.getElementById(config.elementId));
 }
